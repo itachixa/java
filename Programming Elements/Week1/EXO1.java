49e54fe838d7ad79535a6b53df7d1f55ccb30706
itachixa <xaodin12@gmail.comas5921>
2025-03-04 08:07:25 +0100

    correction

--- Programming Elements/Week1/EXO1.java
+++ Programming Elements/Week1/EXO1.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 public class EXO1{
     public static void main(String[] args) {
+    System.out.println("pls enter your burth date ");
     Scanner input = new Scanner(System.in);
     int both_year= input.nextInt();
     System.out.println("Harry's age in 2024 is :"+(2024-both_year));
